a1364ba18412d4df3a1f683257626af538908d47
Emonora <114265017+Emonora@users.noreply.github.com>
2024-11-13 14:45:22 +0000

    changed the page

--- src/app/projects/page.tsx
+++ src/app/projects/page.tsx
@@ -3,7 +3,7 @@
 export default function ProjectsPage() {
   return (
     <main className="bg-gray-500 text-black items-center justify-center h-screen w-screen flex flex-col">
-      <h1>Projects</h1>
+      <h1 className="text-primary text-2xl">Projects</h1>
       <p>Coming soon!</p>
     </main>
   );
