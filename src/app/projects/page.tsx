c881026e65b6ce6ec28106f1331ea40b7953f0f4
Emonora <114265017+Emonora@users.noreply.github.com>
2024-11-12 18:43:19 -0600

    ok buddy

--- src/app/projects/page.tsx
+++ src/app/projects/page.tsx
@@ -1,3 +1,11 @@
+import formbricks from "@formbricks/js";
+if (typeof window !== "undefined") {
+  await formbricks.init({
+    environmentId: "cm3f4992i00074z16pyxq47eg", 
+    apiHost: "https://app.formbricks.com",
+  });
+}
+
 export default function ProjectsPage() {
   return (
     <main className="bg-gray-500 text-black items-center justify-center h-screen w-screen flex flex-col">
